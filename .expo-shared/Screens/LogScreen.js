1dedcaeb8660c93f5ef41482adf90e76b8457b1f
Edem <eddieblack159@gmail.com>
2022-08-29 17:13:31 -0700

    Floating button Feature

--- .expo-shared/Screens/LogScreen.js
+++ .expo-shared/Screens/LogScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons'; 
 import { View } from 'react-native'
-import { TouchableOpacity, useState } from 'react-native';
+import { TouchableOpacity} from 'react-native';
 
 
 
